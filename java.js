49d32618555a25ed3c69c6b60fe3e8e8dd0c78d9
viniciusbraaz44 <viniciuswellington247@gmail.com>
2026-07-10 16:02:57 -0300

    Delete java.js

--- java.js
+++
@@ -1,8 +0,0 @@
-const botao = document.getElementById("botao");
-const versiculo = document.getElementById("versiculo");
-
-botao.addEventListener("click", () => {
-(versiculo).innerHTML =
-'"Porque Deus amou o mundo de tal maneira que deu o seu Filho unigênito, para que todo aquele que nele crê não pereça, mas tenha a vida eterna." <br><strong> João 3:16 </strong>';
-
-});
